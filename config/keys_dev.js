597992c5698ab738b9a0b98557b22a9b40a9e91f
nchristanto <christanto.nanda@gmail.com>
2019-06-22 21:04:10 +0700

    fixing env and mongoDB_URI

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,4 @@
 module.exports = {
-  mongoURI:
-    "mongodb+srv://nchristanto:ASmNlbbgFszoB7gQ@semut-cluster-ctqfj.mongodb.net/test?retryWrites=true&w=majority",
-  secretOrKey: "Secret"
+  mongoURI: process.env.MONGODB_URI,
+  secretOrKey: process.env.SECRET_OR_KEY
 };
