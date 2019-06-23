6a4fdb84d3c2107d3902c98579817e348503005e
nchristanto <christanto.nanda@gmail.com>
2019-06-23 13:21:41 +0700

    create layout for landing

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div className="navbar-fixed">
+        <nav className="z-depth-0">
+          <div className="nav-wrapper white">
+            <Link
+              to="/"
+              style={{
+                fontFamily: "monospace"
+              }}
+              className="col s5 brand-logo center blank-text"
+            >
+              <i className="material-icons">code</i>
+              semut
+            </Link>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
